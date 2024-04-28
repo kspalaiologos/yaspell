c951d89b6311072de8c93aa78a8a7387d0f07180
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-04-28 10:32:24 +0200

    check in chris wellons' optparse

--- shim.h
+++ shim.h
@@ -8,32 +8,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+s32 d_getdelim(u8 ** s, u32 * n, u8 delim, FILE * f);
+u8 * d_strdup(const u8 * s);
 
-s32 d_getdelim(u8 ** s, u32 * n, u8 delim, FILE * f) {
-    If(!s || !n, errno = EINVAL; return -1)
-    If(!*s, *n = 128; *s = malloc(*n); If(!*s, return -1))
-    u32 i = 0;
-    for (int c; (c = fgetc(f)) != EOF;) {
-        If (i + 1 >= *n,
-            u32 m = *n;
-            u8 * t = realloc(*s, *n *= 2);
-            If(!t, return -1)
-            memset((*s = t) + m, 0, m))
-        (*s)[i++] = c;
-        If(c == delim, break)
-    }
-    If(i == 0, return -1)
-    (*s)[i] = 0; return i;
-}
+struct optparse {
+    char **argv;
+    int permute;
+    int optind;
+    int optopt;
+    char *optarg;
+    char errmsg[64];
+    int subopt;
+};
 
-u8 * d_strdup(const u8 * s) {
-    u32 len = strlen(s);
-    u8 * t = malloc(len + 1);
-    If(!t, return NULL)
-    memcpy(t, s, len + 1);
-    return t;
-}
+enum optparse_argtype {
+    OPTPARSE_NONE,
+    OPTPARSE_REQUIRED,
+    OPTPARSE_OPTIONAL
+};
+
+struct optparse_long {
+    const char *longname;
+    int shortname;
+    enum optparse_argtype argtype;
+};
+
+void optparse_init(struct optparse *options, char **argv);
+int optparse(struct optparse *options, const char *optstring);
+int optparse_long(struct optparse *options, const struct optparse_long *longopts, int *longindex);
+char *optparse_arg(struct optparse *options);
 
 #endif
 
